d7a9b91fb29fea63ebd5c6b28315901e6f1c8406
Nivetha Sudhahar <97110775+NivethaSudhahar@users.noreply.github.com>
2024-10-07 23:39:11 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return  <footer className="py-5 bg-dark">
-  <div className="container"><p className="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+  <div className="container"><p className="m-0 text-center text-white">Copyright &copy; Your Website 2024</p></div>
 </footer>
 }
 
